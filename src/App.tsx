919f20502464e156893e90c96bcf2f6b91b3424d
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-18 17:48:37 -0500

    Add state for cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [cards, setCards] = useState<string[]>([]);
+
+  useEffect(() => {
+    setCards(["1", "2", "3"]);
+  }, []);
+
+  return (
+    <>
+      {cards?.map((card, index) => {
+        return <div key={index}>{card}</div>;
+      })}
+    </>
+  );
 }
 
 export default App;
