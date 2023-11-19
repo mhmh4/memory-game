6dd9c101243e75ab089eb6a9f1fb411b8f594af9
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-18 20:32:34 -0500

    Add state for storing which cards were clicked

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const N = 5;
 
 function App() {
   const [cards, setCards] = useState<string[]>([]);
+  const [selections, setSelections] = useState<number[]>([]);
 
   useEffect(() => {
     const newCards = [];
@@ -17,12 +18,24 @@ function App() {
     setCards(newCards);
   }, []);
 
+  function handleClick(index: number) {
+    setSelections((curSelections) => {
+      return [...curSelections, index];
+    });
+  }
+
   return (
     <>
       <div className="bg-slate-800 h-screen">
         <div className="grid grid-cols-5">
           {cards.map((card, index) => {
-            return <Card key={index} value={card}></Card>;
+            return (
+              <Card
+                key={index}
+                value={card}
+                onClick={() => handleClick(index)}
+              ></Card>
+            );
           })}
         </div>
       </div>
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,14 @@
 interface CardProps {
   value: string;
+  onClick: React.MouseEventHandler;
 }
 
 const Card = (props: CardProps) => {
   return (
-    <div className="w-40 p-7 px-10 border rounded border-slate-300 hover:border-slate-400 bg-white hover:bg-slate-50 cursor-pointer">
+    <div
+      className="w-40 p-7 px-10 border rounded border-slate-300 hover:border-slate-400 bg-white hover:bg-slate-50 cursor-pointer"
+      onClick={props.onClick}
+    >
       {props.value}
     </div>
   );
