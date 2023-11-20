ee624ebddfaa79123586ab64988b810bc1cef689
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-19 19:24:42 -0500

    Add isMatched property to Card component

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,17 +2,18 @@ interface CardProps {
   value: string;
   onClick: React.MouseEventHandler;
   isSelected: boolean;
+  isMatched: boolean;
 }
 
 const Card = (props: CardProps) => {
   return (
     <div
       className={`w-40 h-20 p-7 px-10 border rounded border-slate-300 bg-white hover:border-red-500 cursor-pointer ${
-        props.isSelected && "bg-blue-100"
+        props.isMatched && "bg-blue-500"
       }`}
       onClick={props.onClick}
     >
-      {props.isSelected && props.value}
+      {(props.isSelected || props.isMatched) && props.value}
     </div>
   );
 };
